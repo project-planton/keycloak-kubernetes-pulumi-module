d4e7017126dffd3be936a698b1537aa20e807d67
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-02 11:04:41 +0530

    updated project-planton deps that has apis removed from package name

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	keycloakkubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/kubernetes/keycloakkubernetes/v1"
+	keycloakkubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/kubernetes/keycloakkubernetes/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/keycloak-kubernetes-pulumi-module/pkg"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
